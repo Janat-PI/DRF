1157bc4195d81dc58562adb16829e72510bf1526
janat <janat.200066@gmail.com>
2021-06-23 12:03:47 +0600

    Api with DRF

---
+++ main/views.py
@@ -0,0 +1,75 @@
+from rest_framework .decorators import api_view, action
+# from rest_framework.views import APIView
+from .serializers import *
+from rest_framework.response import Response
+from .models import Category, Post
+from rest_framework import generics, viewsets, status
+from django.db.models import Q
+
+
+# @api_view(['GET'])
+# def categories(request):
+#     if request.method == 'GET':
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories, many=True)
+#         return Response(serializer.data)
+#
+#
+# class PostListView(APIView):
+#     def get(self, request):
+#         post = Post.objects.all()
+#         serializer = PostSerializer(post, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         post = request.data
+#         serializer = PostSerializer(data=post)
+#         if serializer.is_valid(raise_exception=True):
+#             post_save = serializer.save()
+#         return Response(serializer.data)
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PostsViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    @action(detail=False, methods=['get'])
+    def search(self, request, pk=None):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) | Q(text__icontains=q))
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# class PostView(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class PostDetailView(generics.RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class PostUpdateView(generics.UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class PostDeleteView(generics.DestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+class PostImageView(generics.ListCreateAPIView):
+    queryset = PostImage.objects.all()
+    serializer_class = PostImageSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
